2ee814d8c29f076c39200001f014b8d87171b09c
Cyrus Olega <cyrusabraham.olega.cics@ust.edu.ph>
2024-02-19 12:35:21 +0800

    modal design started

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Header from "./components/Header.js";
 import Sidebar from "./components/Sidebar.js";
 import Body from "./components/Body.js";
+import Modal from "./components/Modal.js";
 
 function App() {
   return (
     <div className="App">
+      <Modal />
       <Header />
       <div className="sidebarBody">
         <Sidebar />
---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+export default function Modal() {
+  return (
+    <div className="modal">
+      <div className="modal-container">
+        <div className="modal-header">Notifications</div>
+      </div>
+    </div>
+  );
+}
